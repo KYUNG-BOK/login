f03a143786f5b671ee2a47b4cba89fa3eeb90907
kyeongbok <kyeongbok_0627@kakao.com>
2025-08-06 14:43:37 +0900

    구글로그인

--- login.js
+++ login.js
@@ -1,84 +1,145 @@
-const kakaoLoginButton = document.querySelector('#kakao')
-const naverLoginButton = document.querySelector('#naver')
-const userImage = document.querySelector('img')
-const userName = document.querySelector('#user_name')
-const logoutButton = document.querySelector('#logout_button')
+const kakaoLoginButton = document.querySelector('#kakao');
+const naverLoginButton = document.querySelector('#naver');
+const googleLoginButton = document.querySelector('#google');
+const userImage = document.querySelector('img');
+const userName = document.querySelector('#user_name');
+const logoutButton = document.querySelector('#logout_button');
 
-let currentOAuthService = ''
+let currentOAuthService = '';
 
-const kakaoClientId = process.env.REACT_APP_KAKAO_CLIENT_ID;
-const redirectURI = process.env.REACT_APP_REDIRECT_URI;
-let kakaoAccessToken = ''
+const kakaoClientId = window._CONFIG.KAKAO_CLIENT_ID;
+const redirectURI = window._CONFIG.REDIRECT_URI;
 
-const naverClientId = process.env.REACT_APP_NAVER_CLIENT_ID;
-const naverClientSecret = process.env.REACT_APP_NAVER_CLIENT_SECRET;
-const naverSecret = process.env.REACT_APP_NAVER_SECRET;
+const googleClientId = window._CONFIG.GOOGLE_CLIENT_ID;
 
-let naverAccessToken = ''
+const naverClientId = window._CONFIG.NAVER_CLIENT_ID;
+const naverClientSecret = window._CONFIG.NAVER_CLIENT_SECRET;
+const naverSecret = window._CONFIG.NAVER_SECRET;
+
+let kakaoAccessToken = '';
+let naverAccessToken = '';
 
 function renderUserInfo(imgURL, name) {
-  userImage.src = imgURL
-  userName.textContent = name
+  userImage.src = imgURL;
+  userName.textContent = name;
+}
+
+function generateState(service) {
+  return `${service}_${Date.now()}`;
 }
 
 kakaoLoginButton.onclick = () => {
-  location.href = `	https://kauth.kakao.com/oauth/authorize?client_id=${kakaoClientId}&redirect_uri=${redirectURI}&response_type=code`
-}
+  const state = generateState('kakao');
+  localStorage.setItem('oauth_state', state);
+  location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoClientId}&redirect_uri=${redirectURI}&response_type=code&state=${state}`;
+};
 
 naverLoginButton.onclick = () => {
-    location.href = `https://nid.naver.com/oauth2.0/authorize?client_id=${naverClientId}&response_type=code&redirect_uri=${redirectURI}&state=${naverSecret}`
-}
+  const state = generateState('naver');
+  localStorage.setItem('oauth_state', state);
+  location.href = `https://nid.naver.com/oauth2.0/authorize?client_id=${naverClientId}&response_type=code&redirect_uri=${redirectURI}&state=${state}`;
+};
+
+googleLoginButton.onclick = () => {
+  const state = generateState('google');
+  localStorage.setItem('oauth_state', state);
+  const scope = 'profile email';
+  location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleClientId}&redirect_uri=${redirectURI}&response_type=code&scope=${scope}&access_type=offline&state=${state}`;
+};
 
 window.onload = () => {
-  const url = new URL(location.href)
-  const urlParams = url.searchParams
-  const authorizationCode = urlParams.get('code')
-  const naverState = urlParams.get('state')
+  const url = new URL(location.href);
+  const authorizationCode = url.searchParams.get('code');
+  const returnedState = url.searchParams.get('state');
+  const savedState = localStorage.getItem('oauth_state');
 
-  if (authorizationCode) {
-    if (naverState) {
-      axios.post('http://localhost:3000/naver/login', { authorizationCode })
-        .then(res => {
-          naverAccessToken = res.data
-          return axios.post('http://localhost:3000/naver/userinfo', {naverAccessToken})
-        })
-        .then(res => {
-          renderUserInfo(res.data.profile_image, res.data.name)
-          currentOAuthService = 'naver'
-        })
-    } else {
-      axios.post('http://localhost:3000/kakao/login', { authorizationCode })
-        .then(res => {
-          kakaoAccessToken = res.data
-          return axios.post('http://localhost:3000/kakao/userinfo', {kakaoAccessToken})
-        })
-        .then(res => {
-          renderUserInfo(res.data.profile_image, res.data.nickname)
-          currentOAuthService = 'kakao'
-        })
-    }
+  if (!authorizationCode) return;
+
+  if (!returnedState || returnedState !== savedState) {
+    console.error('State 값 불일치');
+    return;
   }
-}
+
+  if (returnedState.startsWith('naver_')) {
+    // 네이버 로그인
+    axios
+      .post('http://localhost:3000/naver/login', { authorizationCode })
+      .then((res) => {
+        naverAccessToken = res.data;
+        return axios.post('http://localhost:3000/naver/userinfo', {
+          naverAccessToken,
+        });
+      })
+      .then((res) => {
+        renderUserInfo(res.data.profile_image, res.data.name);
+        currentOAuthService = 'naver';
+      })
+      .catch((err) => {
+        console.error('네이버 로그인 에러:', err);
+      });
+  } else if (returnedState.startsWith('google_')) {
+    // 구글 로그인
+    axios
+      .post('http://localhost:3000/google/login', { authorizationCode })
+      .then((res) => {
+        const googleAccessToken = res.data;
+        return axios.post('http://localhost:3000/google/userinfo', {
+          googleAccessToken,
+        });
+      })
+      .then((res) => {
+        renderUserInfo(res.data.picture, res.data.name);
+        currentOAuthService = 'google';
+      })
+      .catch((err) => {
+        console.error('구글 로그인 에러:', err);
+      });
+  } else if (returnedState.startsWith('kakao_')) {
+    // 카카오 로그인
+    axios
+      .post('http://localhost:3000/kakao/login', { authorizationCode })
+      .then((res) => {
+        kakaoAccessToken = res.data;
+        return axios.post('http://localhost:3000/kakao/userinfo', {
+          kakaoAccessToken,
+        });
+      })
+      .then((res) => {
+        renderUserInfo(res.data.profile_image, res.data.nickname);
+        currentOAuthService = 'kakao';
+      })
+      .catch((err) => {
+        console.error('카카오 로그인 에러:', err);
+      });
+  } else {
+    console.error('알 수 없는 OAuth 서비스');
+  }
+
+  localStorage.removeItem('oauth_state');
+};
 
 logoutButton.onclick = () => {
   if (currentOAuthService === 'kakao') {
-    axios.delete('http://localhost:3000/kakao/logout', {
-      data : {kakaoAccessToken}
-    })
-      .then(res => {
-        {
-          console.log(res.data)
-          renderUserInfo('', '')
-    }})
+    axios
+      .delete('http://localhost:3000/kakao/logout', {
+        data: { kakaoAccessToken },
+      })
+      .then((res) => {
+        renderUserInfo('', '');
+        currentOAuthService = '';
+      });
   } else if (currentOAuthService === 'naver') {
-    axios.delete('http://localhost:3000/naver/logout', {
-      data : {naverAccessToken}
-    })
-      .then(res => {
-        {
-          console.log(res.data)
-          renderUserInfo('', '')
-    }})
+    axios
+      .delete('http://localhost:3000/naver/logout', {
+        data: { naverAccessToken },
+      })
+      .then((res) => {
+        renderUserInfo('', '');
+        currentOAuthService = '';
+      });
+  } else if (currentOAuthService === 'google') {
+    renderUserInfo('', '');
+    currentOAuthService = '';
+    console.log('구글 로그아웃 (세션만 초기화)');
   }
-
-}
+};
--- server.js
+++ server.js
@@ -1,83 +1,161 @@
-const express = require('express')
-const cors = require('cors')
-const axios = require('axios')
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const qs = require('querystring');
+require('dotenv').config();
 
-const kakaoClientId = process.env.REACT_APP_KAKAO_CLIENT_ID;
-const redirectURI = process.env.REACT_APP_REDIRECT_URI;
+const kakaoClientId = process.env.KAKAO_CLIENT_ID;
+const redirectURI = process.env.REDIRECT_URI;
 
-const naverClientId = process.env.REACT_APP_NAVER_CLIENT_ID;
-const naverClientSecret = process.env.REACT_APP_NAVER_CLIENT_SECRET;
-const naverSecret = process.env.REACT_APP_NAVER_SECRET;
+const naverClientId = process.env.NAVER_CLIENT_ID;
+const naverClientSecret = process.env.NAVER_CLIENT_SECRET;
+const naverSecret = process.env.NAVER_SECRET;
 
-const app = express()
+const googleClientId = process.env.GOOGLE_CLIENT_ID;
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
-app.use(cors({
-  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
-  methods: ["OPTIONS", "POST", "DELETE"],
-}))
+const app = express();
 
-app.use(express.json())
+app.use(
+  cors({
+    origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+    methods: ['OPTIONS', 'POST', 'DELETE'],
+  })
+);
+
+app.use(express.json());
 
 app.post('/kakao/login', (req, res) => {
-  const authorizationCode = req.body.authorizationCode
-  axios.post('https://kauth.kakao.com/oauth/token', {
-    grant_type: 'authorization_code',
-    client_id: kakaoClientId,
-    redirect_uri: redirectURI,
-    code: authorizationCode
-  },
-    {
-      headers: {'Content-type' : 'application/x-www-form-urlencoded;charset=utf-8' }
-    }
-  )
-  .then(response => res.send(response.data.access_token))
-})
+  const authorizationCode = req.body.authorizationCode;
+  axios
+    .post(
+      'https://kauth.kakao.com/oauth/token',
+      qs.stringify({
+        grant_type: 'authorization_code',
+        client_id: kakaoClientId,
+        redirect_uri: redirectURI,
+        code: authorizationCode,
+      }),
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' },
+      }
+    )
+    .then((response) => res.send(response.data.access_token))
+    .catch((err) => {
+      console.error('Kakao Login Error:', err.response?.data || err.message);
+      res.status(500).send('Kakao Login Failed');
+    });
+});
 
 app.post('/naver/login', (req, res) => {
-  const authorizationCode = req.body.authorizationCode
-  axios.post(`https://nid.naver.com/oauth2.0/token?client_id=${naverClientId}&client_secret=${naverClientSecret}&grant_type=authorization_code&state=${naverSecret}&code=${authorizationCode}
-  `)
-  .then(response => res.send(response.data.access_token))
-}) 
+  const authorizationCode = req.body.authorizationCode;
+  axios
+    .post(
+      `https://nid.naver.com/oauth2.0/token?client_id=${naverClientId}&client_secret=${naverClientSecret}&grant_type=authorization_code&state=${naverSecret}&code=${authorizationCode}`
+    )
+    .then((response) => res.send(response.data.access_token))
+    .catch((err) => {
+      console.error('Naver Login Error:', err.response?.data || err.message);
+      res.status(500).send('Naver Login Failed');
+    });
+});
+
+app.post('/google/login', (req, res) => {
+  const authorizationCode = req.body.authorizationCode;
+
+  const data = qs.stringify({
+    code: authorizationCode,
+    client_id: googleClientId,
+    client_secret: googleClientSecret,
+    redirect_uri: redirectURI,
+    grant_type: 'authorization_code',
+  });
+
+  axios
+    .post('https://oauth2.googleapis.com/token', data, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    })
+    .then((response) => res.send(response.data.access_token))
+    .catch((error) => {
+      console.error('Google Login Error:', error.response?.data || error.message);
+      res.status(500).send('Google Login Failed');
+    });
+});
 
 app.post('/kakao/userinfo', (req, res) => {
-  const { kakaoAccessToken } = req.body
-  axios.get('https://kapi.kakao.com/v2/user/me', {
-    headers: {
-      Authorization: `Bearer ${kakaoAccessToken}`,
-      'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-    }
-  })
-  .then(response => res.json(response.data.properties))
-})
-
+  const { kakaoAccessToken } = req.body;
+  axios
+    .get('https://kapi.kakao.com/v2/user/me', {
+      headers: {
+        Authorization: `Bearer ${kakaoAccessToken}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    })
+    .then((response) => res.json(response.data.properties))
+    .catch((err) => {
+      console.error('Kakao UserInfo Error:', err.response?.data || err.message);
+      res.status(500).send('Kakao UserInfo Failed');
+    });
+});
 
 app.post('/naver/userinfo', (req, res) => {
-  const { naverAccessToken } = req.body
-  axios.get('https://openapi.naver.com/v1/nid/me', {
-    headers: {
-      Authorization: `Bearer ${naverAccessToken}`,
-    }
-  })
-  .then(response => res.json(response.data.response))
-})
+  const { naverAccessToken } = req.body;
+  axios
+    .get('https://openapi.naver.com/v1/nid/me', {
+      headers: {
+        Authorization: `Bearer ${naverAccessToken}`,
+      },
+    })
+    .then((response) => res.json(response.data.response))
+    .catch((err) => {
+      console.error('Naver UserInfo Error:', err.response?.data || err.message);
+      res.status(500).send('Naver UserInfo Failed');
+    });
+});
 
+app.post('/google/userinfo', (req, res) => {
+  const { googleAccessToken } = req.body;
+  axios
+    .get('https://www.googleapis.com/oauth2/v2/userinfo', {
+      headers: {
+        Authorization: `Bearer ${googleAccessToken}`,
+      },
+    })
+    .then((response) => res.json(response.data))
+    .catch((error) => {
+      console.error('Google UserInfo Error:', error.response?.data || error.message);
+      res.status(500).send('Google User Info Failed');
+    });
+});
 
 app.delete('/kakao/logout', (req, res) => {
-  const { kakaoAccessToken } = req.body
-  axios.post('https://kapi.kakao.com/v1/user/logout', {}, {
-    headers: { Authorization: `Bearer ${kakaoAccessToken}`}
-  })
-  .then(response => res.send('로그아웃 성공') )
-})
-
+  const { kakaoAccessToken } = req.body;
+  axios
+    .post(
+      'https://kapi.kakao.com/v1/user/logout',
+      {},
+      {
+        headers: { Authorization: `Bearer ${kakaoAccessToken}` },
+      }
+    )
+    .then((response) => res.send('로그아웃 성공'))
+    .catch((err) => {
+      console.error('Kakao Logout Error:', err.response?.data || err.message);
+      res.status(500).send('Kakao Logout Failed');
+    });
+});
 
 app.delete('/naver/logout', (req, res) => {
-  const { naverAccessToken} = req.body
-  axios.post(`https://nid.naver.com/oauth2.0/token?grant_type=delete&client_id=${naverClientId}&client_secret=${naverClientSecret}&access_token=${naverAccessToken}&service_provider=NAVER`)
-  .then(response => res.send('로그아웃 성공') )
-})
+  const { naverAccessToken } = req.body;
+  axios
+    .post(
+      `https://nid.naver.com/oauth2.0/token?grant_type=delete&client_id=${naverClientId}&client_secret=${naverClientSecret}&access_token=${naverAccessToken}&service_provider=NAVER`
+    )
+    .then((response) => res.send('로그아웃 성공'))
+    .catch((err) => {
+      console.error('Naver Logout Error:', err.response?.data || err.message);
+      res.status(500).send('Naver Logout Failed');
+    });
+});
 
-
-
-app.listen(3000, () => console.log('서버 열림!'))
+app.listen(3000, () => console.log('서버 열림!'));
